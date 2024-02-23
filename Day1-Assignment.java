71a8da6c2e3afd92bf6230066f6922ce9e1d7b0c
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2024-02-23 18:51:28 -0500

    Update Day1-Assignment.java

--- Day1-Assignment.java
+++ Day1-Assignment.java
@@ -16,7 +16,7 @@ public class Assignments {
 		// Write a Java program to print the sum of two numbers
 		
 		int num1 = 10;
-		int num2 = 30;
+		int num2 = 20;
 		int result = num1 + num2;
 		
 		System.out.println(result);
